335a2e07a3246c12adf7533315aec2ab0f2e7935
luisadeva <luis.adeva@gmail.com>
2015-09-16 15:12:50 +0200

    config heroku

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express = require('express');
 var path = require('path');
 
 
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public_html')));
 
-app.listen(8000);
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
